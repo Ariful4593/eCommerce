3b00c3b6d6582f44d4e2294205eafc5288e280a0
Ariful4593 <arifkhagoria2015@gmail.com>
2020-06-30 21:22:35 +0600

    Ecommerce Website

---
+++ main.js
@@ -0,0 +1,52 @@
+function Ariful(fname,mname,home,district){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+}
+function Shoraful(fname,mname,home,district){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+}
+function Selim(fname,mname,home,district){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+}
+function Karim(fname,mname,home,district){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+}
+function Nafim(fname,mname,home,district){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+}
+function Singh(fname,mname,home,district,image){
+    this.Father = fname;
+    this.Mother = mname;
+    this.Home = home;
+    this.district = district;
+    this.Image = image;
+}
+
+var myFather = new Ariful('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong');
+var myFather1 = new Shoraful('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong');
+var myFather2 = new Selim('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong');
+var myFather3 = new Karim('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong');
+var myFather4 = new Nafim('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong');
+var myFather5 = new Singh('Shirajul Islam','Shamim Akter','Khagoria', 'Chittagong','images/jacket.jpg');
+// console.log(myFather,myFather1,myFather2);
+x = document.getElementsByClassName("bage").src;
+document.getElementsByClassName("demo").innerHTML = x;
+array = [myFather,myFather1,myFather2,myFather3,myFather4,myFather5];
+for(i=0;i<6;i++)
+{
+    console.log(array[i]);  
+}
